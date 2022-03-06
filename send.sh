52f6e2bd7a35de5963384addd0519acb81d269ea
Alexeev Ivan <boomiwi@gmail.com>
2022-03-06 21:59:20 +0300

    "First working upload"

--- send.sh
+++ send.sh
@@ -2,8 +2,11 @@
 
 MESSAGE="new fix"
 if [[ ! -z $1 ]]; then MESSAGE=$1; fi;
+MESSAGE="\""$MESSAGE"\""
+
+echo $MESSAGE
 
 mdbook build
 git add .	
-git commit -m '$MESSAGE'
+git commit -m "${MESSAGE}"
 git push
